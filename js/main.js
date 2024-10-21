ead15dbedbd191180b807f3e58d7dde96efd2bfc
Goran Stanic <99880680+g-stanic@users.noreply.github.com>
2024-10-21 16:02:01 +0200

    displayProjects

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-import { projects, createProjectCard, displayProjects } from './projects.js';
+import { projects, createProjectCard, displayProjects } from './proj.js';
 import { addLoginButton, createOverlay, showOverlay } from './ui.js';
 import { handleAuthentication } from './auth.js';
 import { checkRepo } from './api.js';
 
 function initializeApp() {
     addLoginButton();
-    displayProjects();
     handleAuthentication();
 
     const addProjectButton = document.getElementById('addProjectButton');
@@ -17,6 +16,8 @@ function initializeApp() {
             checkRepo();
         }
     });
+
+    setTimeout(displayProjects, 100);
 }
 
 document.addEventListener('DOMContentLoaded', initializeApp);
