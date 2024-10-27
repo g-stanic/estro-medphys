245a4244a7252e50b5e624b7a54987d885e28ba3
Goran Stanic <99880680+g-stanic@users.noreply.github.com>
2024-10-27 14:05:33 +0100

    octokit authentication with fine-grained token

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
-export const CLIENT_ID = 'Ov23li90C8HHNhrfSWVH';
+export const GITHUB_CLIENT_ID = 'Ov23li90C8HHNhrfSWVH';
+export const GITHUB_CLIENT_SECRET = '4d3bcba5dcabe465dcb4e5d1619e4eab2ab8bd0f';
 export const REDIRECT_URI = 'https://g-stanic.github.io/estro-medphys/callback.html';
 export const GITHUB_TOKEN = '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';
 export const GITHUB_USERNAME = 'g-stanic';
